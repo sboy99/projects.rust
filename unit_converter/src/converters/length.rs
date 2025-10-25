5720fa6742306dd21ef67220eaab3576967fdff9
sboy99 <shadowboy191@gmail.com>
2025-10-25 23:25:52 +0530

    feat: convert length

--- unit_converter/src/converters/length.rs
+++ unit_converter/src/converters/length.rs
@@ -1,6 +1,15 @@
 use anyhow::Result;
 
 pub fn convert(from: &String, to: &String, value: &f64) -> Result<f64> {
-    println!("Converting length from {} to {}", from, to);
-    Ok(*value)
+    let result = match (from.as_str(), to.as_str()) {
+        ("mm", "cm") => *value / 10.0,
+        ("cm", "mm") => *value * 10.0,
+        ("mm", "m") => *value / 1000.0,
+        ("m", "mm") => *value * 1000.0,
+        ("cm", "m") => *value / 100.0,
+        ("m", "cm") => *value * 100.0,
+        _ if from == to => *value,
+        _ => return Err(anyhow::anyhow!("Invalid length unit")),
+    };
+    Ok(result)
 }
