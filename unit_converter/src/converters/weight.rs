678b17352e886167bf514f0501a18cfa1dfe95bb
sboy99 <shadowboy191@gmail.com>
2025-10-25 23:26:17 +0530

    feat: convert weight

--- unit_converter/src/converters/weight.rs
+++ unit_converter/src/converters/weight.rs
@@ -1,6 +1,15 @@
 use anyhow::Result;
 
 pub fn convert(from: &String, to: &String, value: &f64) -> Result<f64> {
-    println!("Converting weight from {} to {}", from, to);
-    Ok(*value)
+    let result = match (from.as_str(), to.as_str()) {
+        ("kg", "g") => *value * 1000.0,
+        ("g", "kg") => *value / 1000.0,
+        ("kg", "mg") => *value * 1000000.0,
+        ("mg", "kg") => *value / 1000000.0,
+        ("g", "mg") => *value * 1000.0,
+        ("mg", "g") => *value / 1000.0,
+        _ if from == to => *value,
+        _ => return Err(anyhow::anyhow!("Invalid weight unit")),
+    };
+    Ok(result)
 }
